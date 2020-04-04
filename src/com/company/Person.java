ae2fd1c0335aeec53fb339940e7770224f61938d
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-04-04 18:01:16 +0200

    Initial commit

---
+++ src/com/company/Person.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age cannot be lower 0");
+        }
+        this.age = age;
+    }
+}
